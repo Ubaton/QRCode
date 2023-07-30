44dae9d7b15776eddf7ecc493b335088b10d7233
Ubaton <raymondNG729@gmail.com>
2023-07-30 21:44:57 +0200

    v1.0.24

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { NavLink } from "react-router-dom";
 
 function ContactPage({ darkMode }) {
   const handleNavLinkClick = (page) => {};
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your logic here to handle the form submission
+    console.log(formData);
+    // Clear the form fields after submission
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
+
   return (
     <div
-      className={`h-screen text-gray-500 items-center justify-center ${
+      className={`h-screen text-gray-500 ${
         darkMode ? "dark bg-DarkMode-background" : "bg-slate-100"
       }`}
     >
@@ -17,12 +44,12 @@ function ContactPage({ darkMode }) {
           exact
           to="/"
           activeClassName="bg-blue-500 text-white"
-          className=" rounded-md text-gray-500 hover:text-gray-700 hover:bg-gradient-to-r from-gray-100 to-gray-200"
+          className="rounded-md text-gray-500 hover:text-gray-700 hover:bg-gradient-to-r from-gray-100 to-gray-200"
         >
           <ArrowBackIcon />
         </NavLink>
       </Button>
-      <div className="p-5 shadow-xl border rounded-md m-8">
+      <div className="p-5 shadow-xl rounded-md m-8">
         <div className="text-center p-8">
           <h1 className="text-3xl font-semibold mb-4">Contact Us</h1>
           <p className="mb-4">
@@ -30,19 +57,43 @@ function ContactPage({ darkMode }) {
             please feel free to get in touch with us using the contact
             information below.
           </p>
-          <div className="flex flex-col space-y-2">
-            <div>
-              <span className="font-semibold">Email:</span>{" "}
-              cmg@grapgi-designer.com
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col space-y-2">
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Your Name"
+                className="rounded-md p-2"
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Your Email"
+                className="rounded-md p-2"
+                required
+              />
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                placeholder="Your Message"
+                className="rounded-md p-2"
+                required
+              ></textarea>
             </div>
-            <div>
-              <span className="font-semibold">Phone:</span> 079 541 3772
-            </div>
-            <div>
-              <span className="font-semibold">Address:</span> 4912 De Bruyn
-              Street, Kempton Prak, South Africa,1624
-            </div>
-          </div>
+            <Button
+              type="submit"
+              variant="contained"
+              className="mt-4 pt-5 bg-blue-500 text-white"
+            >
+              Submit
+            </Button>
+          </form>
         </div>
       </div>
     </div>
