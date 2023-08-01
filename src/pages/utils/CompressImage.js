95df3a06cf485b3490b0d8d6ed131f43badf4f30
Ubaton <raymondNG729@gmail.com>
2023-08-01 13:38:22 +0200

    v1.0.31

---
+++ src/pages/utils/CompressImage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import SideBar from "../../components/SideBar/SideBar";
+import WebSite from "../../assets/images/SiteConsturction.png";
+
+function CompressImagePage({ darkMode }) {
+  return (
+    <div
+      className={`flex items-center justify-center ${
+        darkMode ? "dark bg-DarkMode-background" : "bg-slate-100"
+      }`}
+    >
+      <div className="fixed left-0">
+        <SideBar darkMode={darkMode} />
+      </div>
+      <div className="items-center justify-center h-screen grid-cols-2 p-8">
+        <img
+          src={WebSite}
+          alt="WebSite Consturction"
+          width="600"
+          height="600"
+          className=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CompressImagePage;
