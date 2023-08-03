201c5994e278a75d0b261399750518a9eb601d5d
Ubaton <raymondNG729@gmail.com>
2023-08-03 12:54:22 +0200

    v1.0.49

--- src/pages/utils/FileConvert/FileConvert.js
+++ src/pages/utils/FileConvert/FileConvert.js
@@ -2,22 +2,24 @@ import React, { useState } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { Button } from "@mui/material";
 import { convertPdfToWord, convertWordToPdf } from "./Convert";
 import SideBar from "../../../components/SideBar/SideBar";
 
 function FileConvertPage({ darkMode }) {
   const [selectedFile, setSelectedFile] = useState(null);
-  const [convertedFileUrl, setConvertedFileUrl] = useState(null);
+  const [convertedFile, setConvertedFile] = useState(null);
 
   const handleFileChange = (e) => {
     setSelectedFile(e.target.files[0]);
+    setConvertedFile(null); // Reset converted file when a new file is chosen
   };
 
   const handlePdfToWord = async () => {
     if (selectedFile && selectedFile.type === "application/pdf") {
       try {
-        const url = await convertPdfToWord(selectedFile);
-        setConvertedFileUrl(url);
+        const convertedBlob = await convertPdfToWord(selectedFile);
+        setConvertedFile(convertedBlob);
         toast.success("PDF converted to Word successfully.");
       } catch (error) {
         toast.error("Failed to convert PDF to Word.");
@@ -34,14 +36,26 @@ function FileConvertPage({ darkMode }) {
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
     ) {
       try {
-        const url = await convertWordToPdf(selectedFile);
-        setConvertedFileUrl(url);
+        const convertedBlob = await convertWordToPdf(selectedFile);
+        setConvertedFile(convertedBlob);
         toast.success("Word converted to PDF successfully.");
       } catch (error) {
         toast.error("Failed to convert Word to PDF.");
       }
     } else {
       toast.error("Please select a valid Word file.");
+    }
+  };
+
+  const handleDownloadConvertedFile = () => {
+    if (convertedFile) {
+      // Create a temporary anchor element to trigger the download
+      const tempAnchor = document.createElement("a");
+      tempAnchor.href = URL.createObjectURL(convertedFile);
+      tempAnchor.download = "converted_file";
+      tempAnchor.click();
+    } else {
+      toast.error("No converted file available for download.");
     }
   };
 
@@ -62,34 +76,66 @@ function FileConvertPage({ darkMode }) {
             }`}
           >
             <h2 className="text-2xl font-bold mb-4">File Converter</h2>
-            <input type="file" onChange={handleFileChange} className="mb-4" />
+            <input
+              disabled={!convertedFile}
+              type="file"
+              onChange={handleFileChange}
+              className="mb-4"
+            />
 
             <div className="flex gap-2">
-              <button
+              <Button
+                disabled={!convertedFile}
+                variant="contained"
                 onClick={handlePdfToWord}
-                className="flex-1 bg-blue-500 hover:bg-blue-600 text-white px-2 py-2 rounded-md"
+                className="flex-1 text-gray-500 rounded-md"
               >
-                Convert PDF to Word
-              </button>
-              <button
+                PDF to Word
+              </Button>
+              <Button
+                disabled={!convertedFile}
+                variant="contained"
                 onClick={handleWordToPdf}
-                className="flex-1 bg-green-500 hover:bg-green-600 text-white px-2 py-2 rounded-md"
+                className="flex-1  text-gray-500 rounded-md"
               >
-                Convert Word to PDF
-              </button>
+                Word to PDF
+              </Button>
             </div>
 
-            {convertedFileUrl && (
-              <div className="mt-4">
-                <a
-                  href={convertedFileUrl}
-                  download="converted_file"
-                  className="flex-1 bg-green-500 hover:bg-green-600 text-white px-2 py-2 rounded-md"
-                >
-                  Download Converted File
-                </a>
+            {convertedFile && (
+              <div className="pt-4">
+                <p className="mb-2">
+                  Converted File Type:{" "}
+                  {convertedFile.type === "application/pdf"
+                    ? "PDF Document"
+                    : "Word Document"}
+                </p>
+                <div className="w-full h-64 border rounded-md overflow-hidden">
+                  <object
+                    data={URL.createObjectURL(convertedFile)}
+                    type={convertedFile.type}
+                    width="100%"
+                    height="100%"
+                  >
+                    <p>Failed to load the converted file.</p>
+                  </object>
+                </div>
               </div>
             )}
+
+            <div className="flex items-center justify-center pt-16">
+              <Button
+                variant="contained"
+                onClick={handleDownloadConvertedFile}
+                className=" bg-green-500 hover:bg-green-600 text-gray-500 px-2 py-2 rounded-md"
+                disabled={!convertedFile}
+              >
+                Download Converted File
+              </Button>
+            </div>
+            <div className="flex items-center justify-center text-green-500 pt-16">
+              <h3 className="text-2xl font-bold mb-4">Coming Soon</h3>
+            </div>
           </div>
         </div>
       </div>
