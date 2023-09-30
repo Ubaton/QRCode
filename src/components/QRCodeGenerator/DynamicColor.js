2ba07114a476df5662cb6e5bd13f1afd50e47580
Ubaton <raymondNG729@gmail.com>
2023-09-30 22:38:14 +0200

    v1.1.165

--- src/components/QRCodeGenerator/DynamicColor.js
+++ src/components/QRCodeGenerator/DynamicColor.js
@@ -1,25 +1,99 @@
-import React, { useState } from "react";
-
-const DynamicColor = ({ initialColor, onColorChange }) => {
-  const [borderColor, setBorderColor] = useState(initialColor);
-
-  const updateBorderColor = (newColor) => {
-    setBorderColor(newColor);
-    // onColorChange(newColor); // Notify the parent component of the color change
-  };
-
-  return (
-    <div className="">
-      <div className="flex flex-row items-center justify-center space-x-2">
-        <label className="block">Choose Dynamic Color:</label>
-        <input
-          type="color"
-          value={borderColor}
-          onChange={(e) => updateBorderColor(e.target.value)}
-        />
-      </div>
-    </div>
-  );
-};
-
+import React from "react";
+import { Col, ColorPicker, Divider, Row, Space } from "antd";
+const DynamicColor = () => (
+  <Space direction="vertical">
+    <Row align="middle">
+      <Space>
+        <span>Dynamic Color : </span>
+        <Col>
+          <ColorPicker
+            styles={{
+              popupOverlayInner: {
+                width: 468 + 24,
+              },
+            }}
+            presets={[
+              {
+                label: "Recommended",
+                colors: [
+                  "#000000",
+                  "#000000E0",
+                  "#000000A6",
+                  "#00000073",
+                  "#00000040",
+                  "#00000026",
+                  "#0000001A",
+                  "#00000012",
+                  "#0000000A",
+                  "#00000005",
+                  "#F5222D",
+                  "#FA8C16",
+                  "#FADB14",
+                  "#8BBB11",
+                  "#52C41A",
+                  "#13A8A8",
+                  "#1677FF",
+                  "#2F54EB",
+                  "#722ED1",
+                  "#EB2F96",
+                  "#F5222D4D",
+                  "#FA8C164D",
+                  "#FADB144D",
+                  "#8BBB114D",
+                  "#52C41A4D",
+                  "#13A8A84D",
+                  "#1677FF4D",
+                  "#2F54EB4D",
+                  "#722ED14D",
+                  "#EB2F964D",
+                ],
+              },
+              {
+                label: "Recent",
+                colors: [
+                  "#F5222D4D",
+                  "#FA8C164D",
+                  "#FADB144D",
+                  "#8BBB114D",
+                  "#52C41A4D",
+                  "#13A8A84D",
+                ],
+              },
+            ]}
+            panelRender={(_, { components: { Picker, Presets } }) => (
+              <div
+                className="custom-panel"
+                style={{
+                  display: "flex",
+                  width: 468,
+                }}
+              >
+                <div
+                  style={{
+                    flex: 1,
+                  }}
+                >
+                  <Presets />
+                </div>
+                <Divider
+                  type="vertical"
+                  style={{
+                    height: "auto",
+                  }}
+                />
+                <div
+                  style={{
+                    width: 234,
+                  }}
+                >
+                  <Picker />
+                </div>
+              </div>
+            )}
+          />
+        </Col>
+      </Space>
+    </Row>
+  </Space>
+);
 export default DynamicColor;
