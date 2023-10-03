b4a08029b54ed6a9e3bf03253596a5978106d5a2
Ubaton <raymondNG729@gmail.com>
2023-10-03 19:49:29 +0200

    v1.1.174

--- src/pages/utils/FileConvert/FileConvert.js
+++ src/pages/utils/FileConvert/FileConvert.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
 import { convertPdfToWord, convertWordToPdf } from "./Convert";
 import SideBar from "../../../components/SideBar/SideBar";
 import PDF from "../../../assets/icons/pdf-96.png";
 import WORD from "../../../assets/icons/word-96.png";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import FileDescription from "./FileDescription";
 
 function FileConvertPage({ darkMode }) {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -68,94 +68,98 @@ function FileConvertPage({ darkMode }) {
 
   return (
     <div
-      className={`flex  items-center justify-center  ${
+      className={`flex overflow-auto items-center justify-center  ${
         darkMode ? "dark bg-DarkMode-background" : "bg-slate-50"
       }`}
     >
       <div className="fixed left-0">
         <SideBar darkMode={darkMode} />
       </div>
-      <div className="pt-28 items-center  text-gray-500 justify-center h-screen grid-cols-2">
+      <div className="pt-16 items-center  text-gray-500 justify-center h-screen grid-cols-2">
         <div className="flex pt-16 items-center justify-center ">
           <div
-            className={`w-92 h-66 p-4 rounded-md shadow-lg ${
+            className={`grid grid-cols-1 md:grid-cols-2 rounded-md shadow-lg ${
               darkMode ? "dark bg-DarkMode-cards" : "bg-slate-100"
             }`}
           >
-            <h2 className="text-2xl font-bold mb-4">File Converter</h2>
-            <input
-              disabled
-              type="file"
-              onChange={handleFileChange}
-              className="mb-4"
-            />
+            <div className="w-92 h-96 p-4 ">
+              <h2 className="text-2xl font-bold mb-4">File Converter</h2>
+              <input
+                disabled
+                type="file"
+                onChange={handleFileChange}
+                className="mb-4"
+              />
 
-            <div className="flex items-center justify-center gap-2">
-              <div>
-                <button
-                  disabled
-                  onClick={handlePdfToWord}
-                  className="bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl  font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 text-white rounded-md p-2 m-2"
-                >
-                  <p className="text-gray-300">PDF to Word</p>
-                </button>
-                <div className="grid grid-cols-3 pl-2 p-1">
-                  <img src={PDF} alt="PDF" width="32" height="32" />
-                  <ArrowRightAltIcon />
-                  <img src={WORD} alt="WORD" width="32" height="32" />
+              <div className="flex items-center justify-center gap-2">
+                <div>
+                  <button
+                    disabled
+                    onClick={handlePdfToWord}
+                    className="bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl  font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 text-white rounded-md p-2 m-2"
+                  >
+                    <p className="text-gray-300">PDF to Word</p>
+                  </button>
+                  <div className="grid grid-cols-3 pl-2 p-1">
+                    <img src={PDF} alt="PDF" width="32" height="32" />
+                    <ArrowRightAltIcon />
+                    <img src={WORD} alt="WORD" width="32" height="32" />
+                  </div>
+                </div>
+                <div>
+                  <button
+                    disabled
+                    onClick={handleWordToPdf}
+                    className="bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl  font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 text-white rounded-md p-2 m-2"
+                  >
+                    <p className="text-gray-300">Word to PDF</p>
+                  </button>
+                  <div className="grid grid-cols-3 pl-2 p-1">
+                    <img src={WORD} alt="WORD" width="32" height="32" />
+                    <ArrowRightAltIcon />
+                    <img src={PDF} alt="PDF" width="32" height="32" />
+                  </div>
                 </div>
               </div>
-              <div>
-                <button
-                  disabled
-                  onClick={handleWordToPdf}
-                  className="bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl  font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 text-white rounded-md p-2 m-2"
-                >
-                  <p className="text-gray-300">Word to PDF</p>
-                </button>
-                <div className="grid grid-cols-3 pl-2 p-1">
-                  <img src={WORD} alt="WORD" width="32" height="32" />
-                  <ArrowRightAltIcon />
-                  <img src={PDF} alt="PDF" width="32" height="32" />
+
+              {convertedFile && (
+                <div className="pt-4">
+                  <p className="mb-2">
+                    Converted File Type:{" "}
+                    {convertedFile.type === "application/pdf"
+                      ? "PDF Document"
+                      : "Word Document"}
+                  </p>
+                  <div className="w-full h-64 border rounded-md overflow-hidden">
+                    <object
+                      data={URL.createObjectURL(convertedFile)}
+                      type={convertedFile.type}
+                      width="100%"
+                      height="100%"
+                    >
+                      <p>Failed to load the converted file.</p>
+                    </object>
+                  </div>
                 </div>
+              )}
+
+              <div className="flex items-center justify-center pt-16">
+                <button
+                  variant="contained"
+                  onClick={handleDownloadConvertedFile}
+                  className="bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 text-white rounded-md p-2 m-2"
+                  disabled
+                >
+                  <p className="text-gray-300">Download Converted File</p>
+                </button>
               </div>
             </div>
-
-            {convertedFile && (
-              <div className="pt-4">
-                <p className="mb-2">
-                  Converted File Type:{" "}
-                  {convertedFile.type === "application/pdf"
-                    ? "PDF Document"
-                    : "Word Document"}
-                </p>
-                <div className="w-full h-64 border rounded-md overflow-hidden">
-                  <object
-                    data={URL.createObjectURL(convertedFile)}
-                    type={convertedFile.type}
-                    width="100%"
-                    height="100%"
-                  >
-                    <p>Failed to load the converted file.</p>
-                  </object>
-                </div>
-              </div>
-            )}
-
-            <div className="flex items-center justify-center pt-16">
-              <button
-                variant="contained"
-                onClick={handleDownloadConvertedFile}
-                className="bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 text-white rounded-md p-2 m-2"
-                disabled
-              >
-                <p className="text-gray-300">Download Converted File</p>
-              </button>
+            <div className="flex items-center justify-center p-4">
+              <FileDescription />
             </div>
           </div>
         </div>
       </div>
-      <ToastContainer />
     </div>
   );
 }
---
+++ src/pages/utils/FileConvert/FileDescription.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Collapse } from "antd";
+
+const itemData = [
+  {
+    key: "1",
+    header: <p className="text-gray-500">PDF to Word Conversion</p>,
+    text: `Our PDF to Word conversion tool lets you easily transform
+            PDFs into editable Word documents. Here's how it works:`,
+    list: [
+      "Upload PDF: Choose your PDF file.",
+      "Conversion: We'll convert it into a Word document.",
+      "Download: Get your Word file for editing.",
+    ],
+    text2:
+      "Now, you can edit PDFs just like Word documents. Simple and efficient.",
+  },
+  {
+    key: "2",
+    header: <p className="text-gray-500">Word to PDF Conversion</p>,
+    text: `Our Word to PDF conversion tool simplifies turning Word documents into PDF files. Here's how it works:`,
+    list: [
+      "Upload Word Document: Choose your Word file.",
+      "Conversion: We'll transform it into a PDF file.",
+      "Download: Get your PDF document ready for use.",
+    ],
+    text2: "Converting Word files to PDF is now quick and straightforward.",
+  },
+  {
+    key: "3",
+    header: <p className="text-gray-500">File Size Limitation</p>,
+    text: `File Size Limit" refers to the maximum allowable size for a file that 
+    can be processed or uploaded. It represents the upper boundary beyond which 
+    the file may not be accepted or processed by a system or application. 
+    Here's how it works:`,
+    list: [
+      "File Size Limit: Any file size is supported.",
+      "Conversion Limit: Only one file can be converted at a time.",
+    ],
+  },
+];
+
+const FileDescription = () => {
+  const onChange = (key) => {};
+
+  return (
+    <div className="text-gray-500">
+      <h1 className="text-2xl font-bold mb-4">Description</h1>
+      <div className="w-80">
+        <Collapse className="" defaultActiveKey={["1"]} onChange={onChange}>
+          {itemData.map((item) => (
+            <Collapse.Panel
+              key={item.key}
+              header={item.header}
+              showArrow={false}
+            >
+              <p>{item.text}</p>
+              <ul className=" p-2 pl-8">
+                {item.list.map((listItem, index) => (
+                  <li className="list-disc" key={index}>
+                    {listItem}
+                  </li>
+                ))}
+              </ul>
+              <p>{item.text2}</p>
+            </Collapse.Panel>
+          ))}
+        </Collapse>
+      </div>
+    </div>
+  );
+};
+
+export default FileDescription;
