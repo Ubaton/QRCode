16401010c0160fe7c01f407389081ed395fb1cb6
Ubaton <raymondNG729@gmail.com>
2023-10-07 15:43:50 +0200

    v1.1.182

--- src/pages/utils/CompressImage/CompressImage.js
+++ src/pages/utils/CompressImage/CompressImage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import SideBar from "../../../components/SideBar/SideBar";
+import ImageDescription from "./ImageDescription";
 
 function CompressImagePage({ darkMode }) {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -88,23 +89,23 @@ function CompressImagePage({ darkMode }) {
 
   return (
     <div
-      className={`flex items-center justify-center ${
+      className={`flex overflow-auto items-center justify-center h-screen ${
         darkMode ? "dark bg-DarkMode-background" : "bg-slate-50"
       }`}
     >
       <div className="fixed left-0">
         <SideBar darkMode={darkMode} />
       </div>
-      <div className="flex  items-center justify-center h-screen text-gray-500 pt-8">
+      <div className="flex items-center justify-center text-gray-500 md:ml-12">
         <div
-          className={`py-4 shadow-lg rounded-md p-4 ${
+          className={`grid grid-cols-1 md:grid-cols-2 py-4 shadow-lg rounded-md p-4 ${
             darkMode ? "dark bg-DarkMode-cards" : "bg-slate-100"
           }`}
         >
-          <h2 className="text-center text-2xl font-bold mb-4">
-            Image Compression
-          </h2>
           <div className="flex flex-col items-center justify-center p-4">
+            <h2 className="text-center text-2xl font-bold mb-4">
+              Image Compression
+            </h2>
             <input
               type="file"
               onChange={handleImageChange}
@@ -118,31 +119,34 @@ function CompressImagePage({ darkMode }) {
             >
               Compress Image
             </button>
+
+            {compressedImage && (
+              <div className="flex-1 justify-center items-center mt-4 p-4">
+                <div className="flex items-center justify-center ">
+                  <img
+                    src={URL.createObjectURL(compressedImage)}
+                    alt="Compressed"
+                    className="w-60 h-60 rounded-md"
+                  />
+                </div>
+
+                <div className="flex justify-center items-center p-2">
+                  <button className="p-2">
+                    <a
+                      href={URL.createObjectURL(compressedImage)}
+                      download
+                      className="bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl font-blod text-sm px-5 py-2.5 text-center mr-2 mb-2 text-white rounded-md p-2 m-2"
+                    >
+                      Download Compressed Image
+                    </a>
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
-
-          {compressedImage && (
-            <div className="flex-1 justify-center items-center mt-4 p-4">
-              <div className="flex items-center justify-center ">
-                <img
-                  src={URL.createObjectURL(compressedImage)}
-                  alt="Compressed"
-                  className="max-w-full h-96 rounded-md"
-                />
-              </div>
-
-              <div className="flex justify-center items-center p-2">
-                <button className="p-2">
-                  <a
-                    href={URL.createObjectURL(compressedImage)}
-                    download
-                    className="bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl font-blod text-sm px-5 py-2.5 text-center mr-2 mb-2 text-white rounded-md p-2 m-2"
-                  >
-                    Download Compressed Image
-                  </a>
-                </button>
-              </div>
-            </div>
-          )}
+          <div className="flex items-center justify-center p-4">
+            <ImageDescription />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/pages/utils/CompressImage/ImageDescription.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { Collapse } from "antd";
+
+const itemData = [
+  {
+    key: "1",
+    header: <p className="text-gray-500">Effortless Optimization</p>,
+    text: (
+      <p className=" font-semibold ">
+        Our image compression tool offers effortless optimization for PNG and
+        JPEG images. Whether you are a web developer, designer, or anyone
+        concerned about reducing image file sizes while preserving quality, our
+        service simplifies the process. You can achieve optimal image
+        compression without a steep learning curve.
+      </p>
+    ),
+    list: [
+      "User-Friendly Interface",
+      "Bulk Processing",
+      "Preservation of Image Quality",
+    ],
+  },
+  {
+    key: "2",
+    header: <p className="text-gray-500">Improved Website Performance</p>,
+    text: (
+      <p className=" font-semibold ">
+        By using our tool, you can significantly reduce image file sizes without
+        compromising visual quality. This leads to faster website loading times,
+        which is crucial for providing a smooth user experience. Additionally,
+        reduced bandwidth usage benefits both your website's performance and
+        your audience's data consumption.
+      </p>
+    ),
+    list: [
+      "Faster Loading Times",
+      "Bandwidth Savings",
+      "Enhanced User Experience",
+    ],
+  },
+  {
+    key: "3",
+    header: <p className="text-gray-500">utomatic Analysis and Optimization</p>,
+    text: (
+      <p className=" font-semibold ">
+        Our image compression tool streamlines the process of image
+        optimization. When you upload PNG or JPEG images, our advanced
+        compression algorithm automatically analyzes and optimizes them. You
+        don't need to manually tweak settings or spend excessive time on
+        optimization. Enjoy the best results with minimal effort, making your
+        web projects faster and more efficient. Start using our smart image
+        compression tool today to save time and resources.
+      </p>
+    ),
+    list: ["Time Savings", "Consistency", "Efficiency"],
+  },
+];
+
+const ImageDescription = () => {
+  const onChange = (key) => {};
+
+  return (
+    <div className="text-gray-500">
+      <h1 className="text-2xl font-bold mb-4">Description</h1>
+      <h2>Smart PNG and JPEG Compression</h2>
+      <div className="w-80">
+        <Collapse className="" defaultActiveKey={["1"]} onChange={onChange}>
+          {itemData.map((item) => (
+            <Collapse.Panel
+              key={item.key}
+              header={item.header}
+              showArrow={false}
+            >
+              <p>{item.text}</p>
+              <ul className=" p-2 pl-8">
+                {item.list.map((listItem, index) => (
+                  <li className="list-disc" key={index}>
+                    {listItem}
+                  </li>
+                ))}
+              </ul>
+            </Collapse.Panel>
+          ))}
+        </Collapse>
+      </div>
+    </div>
+  );
+};
+
+export default ImageDescription;
