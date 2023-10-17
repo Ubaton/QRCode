6498c0e0418f9c7bca8f381654d939e94cab6f20
Ubaton <raymondNG729@gmail.com>
2023-10-17 10:42:40 +0200

    v1.1.194

--- src/pages/utils/CookieConsent.js
+++ src/pages/utils/CookieConsent.js
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 const CookieConsent = ({ acceptCookies }) => {
@@ -14,20 +15,21 @@ const CookieConsent = ({ acceptCookies }) => {
 
   return (
     <div
-      className={`fixed bottom-4 left-4 p-2 w-40 bg-gray-800 text-white rounded-tl-md rounded-bl-md ${
+      className={`fixed bottom-4 left-4 p-2 w-40 bg-gray-200 text-gray-500 rounded-md ${
         isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
       } transition-opacity ease-in-out duration-300`}
     >
-      <div className="flex items-center">
-        <p className="mr-2">
+      <div className="flex flex-row items-center">
+        <p className=" text-sm md:text-md mr-2">
           This website uses cookies to improve your experience.
         </p>
-        <button
+        <Button
+          variant="outlined"
           onClick={acceptCookies}
-          className="underline text-blue-300 focus:outline-none"
+          className="underline text-blue-500 focus:outline-none"
         >
           Accept
-        </button>
+        </Button>
       </div>
     </div>
   );
