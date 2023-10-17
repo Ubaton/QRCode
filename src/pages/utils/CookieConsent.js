21bae2584c2f082ebcfcc1a7f4df144fc88a67c5
Ubaton <raymondNG729@gmail.com>
2023-10-17 10:52:39 +0200

    v1.1.196

--- src/pages/utils/CookieConsent.js
+++ src/pages/utils/CookieConsent.js
@@ -15,12 +15,12 @@ const CookieConsent = ({ acceptCookies }) => {
 
   return (
     <div
-      className={`fixed bottom-4 left-4 p-2 bg-gray-200 text-gray-500 rounded-md ${
+      className={`fixed bottom-4 p-2 bg-gray-200 text-gray-500 rounded-md ${
         isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
       } transition-opacity ease-in-out duration-300`}
     >
       <div className="flex flex-row items-center">
-        <p className=" text-sm md:text-md mr-2">
+        <p className="text-xs md:text-md mr-2">
           This website uses cookies to improve your experience.
         </p>
         <Button
