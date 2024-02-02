e4576591917b81d515a08bf146cb845f4224aa6f
Ubaton <raymondNG729@gmail.com>
2024-02-02 20:15:50 +0200

    v1.2.242 Landing Page UI update and Loading improvement.

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import PathIcon from "../assets/path.png";
+import SpinningGeometric from "./pattens/SpinningGeometric";
 
 const LandingPage = ({ darkMode }) => {
   return (
@@ -9,26 +10,34 @@ const LandingPage = ({ darkMode }) => {
         darkMode ? "dark bg-DarkMode-background" : "bg-slate-50"
       }`}
     >
-      <div className="hidden lg:block fixed left-20 top-0">
-        <img src={PathIcon} alt="" width={430} />
-      </div>
-      <h1 className="text-4xl md:text-7xl text-center font-extrabold mb-6 text-gray-50">
-        <span className="bg-gradient-to-r from-blue-400 via-blue-600 to-blue-900 bg-clip-text text-transparent">
-          Generate
-        </span>{" "}
-        <span className=" text-gray-500">
-          Your <br /> Designed QR Code
-        </span>
-      </h1>
-      <p className="text-lg text-gray-400 mb-8 text-center">
-        Tailor-Made Connectivity for Your Distinct Identity.
-      </p>
-      <div className="flex flex-col items-center gap-4">
-        <NavLink to="/home">
-          <button className="bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium text-lg px-8 py-3 text-white rounded-md">
-            Get Started
-          </button>
-        </NavLink>
+      <SpinningGeometric />
+      <div className="fixed top-46">
+        <div className="hidden lg:block fixed left-20 top-0">
+          <img src={PathIcon} alt="" width={430} />
+        </div>
+        <div className="h-full w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-[0.05] p-2 mb-2">
+          <h1 className="text-4xl md:text-7xl text-center font-extrabold mb-6 text-gray-50">
+            <span className="bg-gradient-to-r from-blue-400 via-blue-600 to-blue-900 bg-clip-text text-transparent ">
+              Generate
+            </span>{" "}
+            <span className=" text-gray-500">
+              Your <br /> Designed QR Code
+            </span>
+          </h1>
+        </div>
+        <div className="h-full w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-[0.05]">
+          <p className="text-lg text-gray-400 mb-8 text-center">
+            Tailor-Made Connectivity for Your Distinct Identity.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center gap-4">
+          <NavLink to="/home">
+            <button className="bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium text-lg px-8 py-3 text-white rounded-md">
+              Get Started
+            </button>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
