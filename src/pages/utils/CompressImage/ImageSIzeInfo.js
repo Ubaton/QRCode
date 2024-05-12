175d17e0cbe52ad0bdf191d0ee9c8cd83ac7f481
Ubaton <raymondng729@gmail.com>
2024-05-12 21:32:16 +0200

    v1.2.255 Display of initial and after of the image Megabits.

--- src/pages/utils/CompressImage/ImageSIzeInfo.js
+++ src/pages/utils/CompressImage/ImageSIzeInfo.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-function rederImageSizeInfo(initialSize, compressedSizze) {
+function rederImageSizeInfo(initialSize, compressedSize) {
   return (
     <div className=" flex flex-row p-2 space-x-2">
       <span className="flex justify-center items-center flex-col text-gray-50">
-        <p className="bg-sky-500 rounded-md p-1">{initialSize}</p>
+        <p className="bg-sky-500 rounded-md p-1">
+          {initialSize ? initialSize : "0 KB/0 MB"}
+        </p>
         <p>Initial</p>
       </span>
 
       <span className="flex justify-center items-center flex-col text-gray-50">
-        <p className="bg-sky-500 rounded-md p-1">{compressedSizze}</p>
+        <p className="bg-sky-500 rounded-md p-1">
+          {compressedSize ? compressedSize : "0 KB/0 MB"}
+        </p>
         <p>After</p>
       </span>
     </div>
